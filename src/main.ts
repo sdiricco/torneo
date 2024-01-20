a40f61669a5237aa06fdb2ed0808931ec29fe6d1
Simone <sdiricco@gmail.com>
2024-01-20 11:40:48 +0100

    remove service worker

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-import "./registerServiceWorker";
 import "./main.css";
 
 /*****************************************************************************/
