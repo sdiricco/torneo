4e669b6460a6b19e7bc83843dcadf10d62e8d1c0
Simone <sdiricco.91@gmail.com>
2024-02-01 21:11:32 +0100

    add some fix

--- src/components/shared/Tabs.vue
+++ src/components/shared/Tabs.vue
@@ -5,11 +5,14 @@
     :model="items"
     :pt="{
       menu: {
-        class: 'bg-primary-50 justify-content-center',
+        class: 'bg-primary-50 justify-content-evenly',
+      },
+      menuitem: {
+        class: 'w-full',
       },
     }">
     <template #item="{ item, props }">
-      <a v-ripple v-bind="props.action" class="flex flex-column align-items-center gap-2 bg-primary-50 py-3 px-2 border-none">
+      <a v-ripple v-bind="props.action" class="flex flex-column align-items-center gap-2 bg-primary-50 py-4 px-2 border-none w-full">
         <font-awesome-icon :icon="item.icon" />
         <span v-bind="props.label">{{ item.label }}</span>
       </a>
