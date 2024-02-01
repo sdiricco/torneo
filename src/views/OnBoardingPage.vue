5c41255b04547607e31aa01db81da293430a618a
mpellegrini88 <marco.pellegrini@movesolutions.it>
2024-02-01 16:45:35 +0100

    added bg style

--- src/views/OnBoardingPage.vue
+++ src/views/OnBoardingPage.vue
@@ -1,13 +1,8 @@
 <template>
-  <div class="surface-ground p-4 h-full flex align-items-center justify-content-center">
-    <div class="flex align-items-center justify-content-center">
-      <Card>
-        <template #content>
-          <div class="flex align-items-center justify-content-center p-4">
-            <Image :src="image" alt="logo" style="width: 100%" />
-          </div>
-        </template>
-      </Card>
+  <div class="p-4 h-full flex justify-content-center bg-container">
+    <img class="bg-image" :src="imageBg" alt="background" />
+    <div class="flex h-13rem justify-content-center">
+      <Image :src="image" alt="logo" style="width: 70%" />
     </div>
   </div>
 </template>
@@ -16,10 +11,33 @@
 import { onMounted } from "vue";
 import router from "@/router";
 const image = "assets/logo_aicslucca.png";
+const imageBg = "assets/icon/bg-image.png"
 
-onMounted(() => {
-  setTimeout(() => {
-    router.push({ name: "TournamentsPage" });
-  }, 2000);
-});
+// onMounted(() => {
+//   setTimeout(() => {
+//     router.push({ name: "TournamentsPage" });
+//   }, 2000);
+// });
 </script>
+
+<style scoped>
+.bg-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.bg-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0.1;
+}
+
+.bg-color {
+  background: linear-gradient(to bottom, #4c65abbb, #4c65ab);
+}
+</style>
