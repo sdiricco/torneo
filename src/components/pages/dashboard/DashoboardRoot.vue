49c3bc7d750611ffa26c299962a012983b1f8c7b
Simone <sdiricco.91@gmail.com>
2024-02-07 12:18:20 +0100

    fix some ux

--- src/components/pages/dashboard/DashoboardRoot.vue
+++ src/components/pages/dashboard/DashoboardRoot.vue
@@ -4,12 +4,12 @@
       <font-awesome-icon icon="fa-solid fa-trophy" class="mr-4"></font-awesome-icon>
       <div class="">Classifica</div>
     </div>
-    <FirstTeams :teams="getTeamsRanking" :max-teams-visible="3" class="m-4" @goToRankingPage="emit('goToRankingPage')" />
+    <FirstTeams :teams="getTeamsRanking" :max-teams-visible="4" class="m-4" @goToRankingPage="emit('goToRankingPage')" />
     <div class="text-xl font-bold px-4 pt-4 flex align-items-center">
       <font-awesome-icon icon="fa-solid fa-people-group" class="mr-4"></font-awesome-icon>
       <div>Classifica marcatori</div>
     </div>
-    <FirstPlayers :players="playersStats" :max-players-visible="3" class="m-4" @go-to-ranking-page="emit('goToPlayersPage')" />
+    <FirstPlayers :players="playersStats" :max-players-visible="4" class="m-4" @go-to-ranking-page="emit('goToPlayersPage')" />
     <div v-if="getLatestMatchResults.length" class="p-4 surface-card mb-4">
       <div class="text-2xl font-bold text-color border-bottom-1 surface-border pb-4 mb-4">Ultimi risultati</div>
       <MatchItem v-for="(match, idx) in getLatestMatchResults" :match="match" :key="idx" class="mb-4 pb-4 border-bottom-1 surface-border"></MatchItem>
--- src/components/pages/dashboard/FirstTeams.vue
+++ src/components/pages/dashboard/FirstTeams.vue
@@ -1,27 +1,32 @@
 <template>
   <AppCard>
-    <div class="grid grid-nogutter surface-border border-bottom-1 pb-4">
-      <div class="col-1"></div>
-      <div class="col-10">
-        <div class="font-bold">Squadra</div>
+    <div class="content">
+      <div class="gradient-text">
+        <div class="grid grid-nogutter surface-border border-bottom-1 pb-3">
+          <div class="col-1"></div>
+          <div class="col-10">
+            <div class="font-bold">Squadra</div>
+          </div>
+          <div class="col-1">
+            <div class="font-bold">PT</div>
+          </div>
+        </div>
+        <div class="grid grid-nogutter pt-3" v-for="(t, idx) in teamsFiltered" :key="t.name">
+          <div class="col-1">{{ idx + 1 }}</div>
+          <div class="col-10">{{ t.name }}</div>
+          <div class="col-1">{{ t.points }}</div>
+        </div>
       </div>
-      <div class="col-1">
-        <div class="font-bold">PT</div>
+
+      <div class="flex justify-content-end">
+        <Button
+          class="px-0 pb-0 mt-3 text-primary"
+          icon="pi pi-arrow-right"
+          iconPos="right"
+          label="Classifica completa"
+          link
+          @click="emit('goToRankingPage')"></Button>
       </div>
-    </div>
-    <div class="grid grid-nogutter pt-4" v-for="(t, idx) in teamsFiltered" :key="t.name">
-      <div class="col-1">{{ idx + 1 }}</div>
-      <div class="col-10">{{ t.name }}</div>
-      <div class="col-1">{{ t.points }}</div>
-    </div>
-    <div class="flex justify-content-end">
-      <Button
-        class="px-0 pb-0 mt-4 text-primary"
-        icon="pi pi-arrow-right"
-        iconPos="right"
-        label="Classifica completa"
-        link
-        @click="emit('goToRankingPage')"></Button>
     </div>
   </AppCard>
 </template>
@@ -41,3 +46,26 @@ const teamsFiltered = computed(() => props.teams.filter((t, idx) => idx < props.
 
 const emit = defineEmits(["goToRankingPage"]);
 </script>
+
+<style scoped>
+.content {
+  position: relative;
+  z-index: 1;
+}
+
+.gradient-text {
+  position: relative;
+}
+
+.gradient-text::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 50%, rgba(255, 255, 255, 0.8) 100%);
+  z-index: 2;
+  pointer-events: none;
+}
+</style>
